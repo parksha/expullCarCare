44b8c0824463eb510ce0ebc8b0b3805a2177cd58
Jeong_Suk <jszzang9@expull.com>
2014-01-10 23:31:41 +0900

    테스

--- EXPULL_CARCARE/src/carcare/test/HelloSpring.java
+++ EXPULL_CARCARE/src/carcare/test/HelloSpring.java
@@ -3,6 +3,6 @@ package carcare.test;
 public class HelloSpring {
 	
 	public static void main(String args[]){
-		System.out.println("test 하고 싶어요");
+		System.out.println("test 하고 싶어요 난 시러요");
 	}
 }
